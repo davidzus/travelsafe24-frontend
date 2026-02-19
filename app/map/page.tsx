620788d83e3e41797605ff23d6ab378af75bad84
david.laskowski <david.laskowski@reisswolf.com>
2026-02-19 15:03:44 +0100

    Added a simple Map of Hamburg, Added districts boundaries display to Map

---
+++ app/map/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react';
+import Map from "@/components/Map";
+
+function Page() {
+    return (
+        <div>
+            <Map/>
+        </div>
+    );
+}
+
+export default Page;
